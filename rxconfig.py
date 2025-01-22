a44c1202b70f5e6b3f97fdac14a8d9d1b553bf80
IM-THE-GRASS <thegrassmanreplit@gmail.com>
2025-01-22 14:21:10 -0700

    I JUST KILLED THE PYTHON

--- rxconfig.py
+++
@@ -1,8 +0,0 @@
-import reflex as rx
-
-config = rx.Config(
-    app_name="movie_streaming_site",
-    frontend_port=60769,
-    backend_port=40167,
-    api_url="https://openstreamapi.thegrass.xyz"
-)
